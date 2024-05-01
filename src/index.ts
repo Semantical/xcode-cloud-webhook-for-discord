0fc4044964359eab937451f31821e827fe906e8d
Valentin <36477338+ValentinWalter@users.noreply.github.com>
2024-05-01 16:59:54 +0200

    Make embeds prettier

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,30 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const sendDiscordNotification = async (content: String, embeds: any) => {
+const assets = {
+	now: {
+		emote: null,
+		image: "https://cdn.discordapp.com/emojis/1235233686471970877.webp?size=240&quality=lossless",
+	},
+	now_dev: {
+		emote: null,
+		image: "https://cdn.discordapp.com/emojis/1235233685209485312.webp?size=240&quality=lossless",
+	},
+	today: {
+		emote: null,
+		image: "https://cdn.discordapp.com/emojis/1235233689512710284.webp?size=240&quality=lossless",
+	},
+	today_dev: {
+		emote: null,
+		image: "https://cdn.discordapp.com/emojis/1235233687881125929.webp?size=240&quality=lossless",
+	},
+};
+
+const sendDiscordNotification = async (embed: any, silent: boolean = false) => {
 	try {
 		const response = await axios.post(
 			`${process.env.DISCORD_WEBHOOK_URL}?wait=true`,
-			{ content, embeds }
+			{ content: silent ? "@silent" : undefined, embeds: [embed] }
 		);
 		console.log("Discord response:", response.status, response.statusText);
 		if (response.status !== 200) {
@@ -26,6 +45,40 @@ const sendDiscordNotification = async (content: String, embeds: any) => {
 		return false;
 	}
 	return true;
+};
+
+const getDiscordEmbed = (
+	app: string,
+	title: string,
+	description: string | undefined,
+	footer: string,
+	url: string,
+	type: "success" | "failure"
+) => {
+	return {
+		author: {
+			name: app,
+			icon_url: getEmojiForApp(app)?.image,
+		},
+		title,
+		description,
+		url,
+		color: type == "success" ? 0x34c759 : 0xff3b30,
+		footer: { text: footer },
+	};
+};
+
+const getEmojiForApp = (appName: string) => {
+	if (appName.includes("Now Dev")) {
+		return assets.now_dev;
+	} else if (appName.includes("Today Dev")) {
+		return assets.today_dev;
+	} else if (appName.includes("Now")) {
+		return assets.now;
+	} else if (appName.includes("Today")) {
+		return assets.today;
+	}
+	return null;
 };
 
 app.post("/", async (req, res) => {
@@ -57,24 +110,31 @@ app.post("/", async (req, res) => {
 	}
 
 	const appName = ciProduct?.attributes?.name ?? "-";
+	const buildURL = `https://appstoreconnect.apple.com/teams/${process.env.XCODE_CLOUD_TEAM_ID}/apps/${app.id}/ci/builds/${buildId}/summary`;
 
 	if (completionStatus !== "SUCCEEDED") {
 		console.log("Build failed");
-		const sent = await sendDiscordNotification("❌ Build failed", [
-			{
-				title: `${appName || "-"} | Build ${buildNumber || ""}`,
-				url: `https://appstoreconnect.apple.com/teams/${process.env.XCODE_CLOUD_TEAM_ID}/apps/${app.id}/ci/builds/${buildId}/summary`,
-			},
-		]);
+		const embed = getDiscordEmbed(
+			appName,
+			"❌ Build failed",
+			undefined,
+			`Build ${buildNumber}`,
+			buildURL,
+			"failure"
+		);
+		const sent = await sendDiscordNotification(embed);
 		return res.status(sent ? 200 : 500).send("Notification sent");
 	}
 
-	const sent = await sendDiscordNotification("Build completed! ⛏️", [
-		{
-			title: `${appName} (TestFlight)`,
-			url: `itms-beta://beta.itunes.apple.com/v1/app/${app.id}`,
-		},
-	]);
+	const embed = getDiscordEmbed(
+		appName,
+		"✅ Build succeeded",
+		`[TestFlight](itms-beta://beta.itunes.apple.com/v1/app/${app.id})`,
+		`Build ${buildNumber}`,
+		buildURL,
+		"success"
+	);
+	const sent = await sendDiscordNotification(embed);
 	return res.status(sent ? 200 : 500).send("Notification sent");
 });
 
